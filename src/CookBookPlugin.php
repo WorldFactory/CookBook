2f410945121377e129ec0ed89bc01479e0ea2753
Raphaël Aurières <raphael.aurieres@gmail.com>
2019-12-15 15:40:21 +0100

    Upgrade version 0.3.1 -> 0.3.2

--- src/CookBookPlugin.php
+++ src/CookBookPlugin.php
@@ -23,7 +23,7 @@ class CookBookPlugin implements PluginInterface, EventSubscriberInterface
 {
     public static $isHeaderDisplayed = false;
 
-    const VERSION = 'v0.3.1';
+    const VERSION = 'v0.3.2';
 
     /**
      * @var Composer
